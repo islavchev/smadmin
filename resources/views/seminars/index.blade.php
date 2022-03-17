1363960c9121b87c614b03e697a0121ebbe78dec
islavchev <i.a.slavchev@gmail.com>
2022-03-17 09:09:56 +0200

    Pagination in seminars.index

--- resources/views/seminars/index.blade.php
+++ resources/views/seminars/index.blade.php
@@ -15,28 +15,28 @@
         @endif --}}
     </div>
         @if ($seminars->count())
-        <div class="row align-items-center">
-            <div class="col-1 text-right border">
-                <strong>No</strong>
-            </div>
-            <div class="col border border-start-0">
-                <strong>Зала</strong>
-            </div>
-            <div class="col-2 border border-start-0 text-center">
-                <strong>Капацитет</strong>
-            </div>
-            <div class="col-2 border border-start-0 text-center">
-                <strong>Интернет</strong>
-            </div>
-            <div class="col-2 border border-start-0 text-center">
-                <strong>Мултимедия</strong>
-            </div>
-            {{-- @if (isset(Auth::user()->id)) --}}
-                <div class="col-1 text-left border border-start-0">
-                <strong> Редакция</strong>
+            <div class="row align-items-center">
+                <div class="col-1 text-right border">
+                    <strong>No</strong>
                 </div>
-            {{-- @endif                                        --}}
-        </div>    
+                <div class="col border border-start-0">
+                    <strong>Зала</strong>
+                </div>
+                <div class="col-2 border border-start-0 text-center">
+                    <strong>Капацитет</strong>
+                </div>
+                <div class="col-2 border border-start-0 text-center">
+                    <strong>Интернет</strong>
+                </div>
+                <div class="col-2 border border-start-0 text-center">
+                    <strong>Мултимедия</strong>
+                </div>
+                {{-- @if (isset(Auth::user()->id)) --}}
+                    <div class="col-1 text-left border border-start-0">
+                    <strong> Редакция</strong>
+                    </div>
+                {{-- @endif                                        --}}
+            </div>    
         @else
             <strong>Няма въведени зали</strong>
         @endif
@@ -66,7 +66,7 @@
                                 href="{{route('rooms.edit', $seminar->id)}}">
                                 <span class="material-icons md-18">edit</span>
                             </a>
-                            <form action="{{ route('rooms.destroy', $seminar->id) }}" class="" method="POST">
+                            <form action="{{ route('seminars.destroy', $seminar->id) }}" class="" method="POST">
                                 @csrf
                                 @method('delete')
                                 <button 
@@ -80,6 +80,10 @@
                 </div>
             </div>
         @endforeach
-   </div>
+    <div class="row mt-3 ms-auto">
+        <div class="col-4">
+        {{ $seminars->links("pagination::bootstrap-4") }}
+        </div>
+    </div>
 </div>
 @endsection
