1937f9daa9dbbf149a4fe725084961bbb3450517
Ivan Sl <i.a.slavchev@gmail.com>
2022-03-19 17:12:04 +0000

    Minor changes in rooms.show

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -18,36 +18,36 @@
 </div>
 
 <div class="row">
-    <div class="col-2"><strong>Име на залата:</strong></div>
-    <div class="col-10">{{$room->room_name}}</div>
+    <div class="col-md-2"><strong>Име на залата:</strong></div>
+    <div class="col-md-10">{{$room->room_name}}</div>
 </div>
 <div class="row">
-    <div class="col-2"><strong>Капацитет:</strong></div>
-    <div class="col-10">{{$room->capacity}} студенти</div>
+    <div class="col-md-2"><strong>Капацитет:</strong></div>
+    <div class="col-md-10">{{$room->capacity}} студенти</div>
 </div>
 <div class="row">
-    <div class="col-2"><strong>Интернет достъп:</strong></div>
-    <div class="col-10">{{config('enums.rooms_internet')[$room->internet]}}</div>
+    <div class="col-md-2"><strong>Интернет достъп:</strong></div>
+    <div class="col-md-10">{{config('enums.rooms_internet')[$room->internet]}}</div>
 </div>
 <div class="row">
-    <div class="col-2"><strong>Мултимедия:</strong></div>
-    <div class="col-10">{{config('enums.rooms_multimedia')[$room->multimedia]}}</div>
+    <div class="col-md-2"><strong>Мултимедия:</strong></div>
+    <div class="col-md-10">{{config('enums.rooms_multimedia')[$room->multimedia]}}</div>
 </div>
 @if ($room->notes)    
     <div class="row">
-        <div class="col-2"><strong>Забележки:</strong></div>
-        <div class="col-10">{{$room->notes}}</div>
+        <div class="col-md-2"><strong>Забележки:</strong></div>
+        <div class="col-md-10">{{$room->notes}}</div>
     </div>
 @endif
 <div class="row">
-    <div class="col-2"><Strong>Дисциплини в залата:</Strong></div>
-    <div class="col-10">
+    <div class="col-md-2"><Strong>Дисциплини в залата:</Strong></div>
+    <div class="col-md-10">
         @foreach ($room->seminars->unique('subject_id') as $seminar)
-            <div class="row">
-                <div class="col">
+            <div class="row border-bottom">
+                <div class="col-md-8">
                     {{$seminar->subject->name.' ('.$seminar->subject->code.')'}}
                 </div>
-                <div class="col">
+                <div class="col-md-4">
                     {{config('enums.acad_positions')[$seminar->academic->acad_position].' '.$seminar->academic->first_name.' '.$seminar->academic->last_name.' '.config('enums.acad_titles')[$seminar->academic->acad_title]}}
                 </div>
             </div>
