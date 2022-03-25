312f54690656e1e8923d33b3bee60885d1603dd8
Ivan Sl <i.a.slavchev@gmail.com>
2022-03-25 21:07:10 +0000

    Fix responsive design for seminar choose

--- resources/views/semester/choose.blade.php
+++ resources/views/semester/choose.blade.php
@@ -13,18 +13,18 @@
             <h5>Изберете година и семестър</h5>
         </div>
         <div class="row my-2">
-            <div class="col-2">
+            <div class="col-6 col-md-2">
                 <input type="number" name="year" id="year" value="2022" min="1990" max="2200" class="form-control form-control-sm">
             </div>
-            <div class="col-2">
+            <div class="col-6 col-md-2">
                 <select name="semester" id="semester" class="form-select form-select-sm">
                     <option value="1">Летен</option>
                     <option value="2">Зимен</option>
                 </select>
             </div>
         </div>
-        <div class="row">
-            <button type="submit" class="btn btn-success btn-sm mt-3 col-4">
+        <div class="row justify-content-center">
+            <button type="submit" class="btn btn-success btn-sm mt-3 col-11 col-md-4">
                 Покажи
             </button>  
         </div>  
