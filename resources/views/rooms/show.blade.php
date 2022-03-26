93c5f85eb8ff3926f3a652ff99cbdbe2a8591500
Ivan Sl <i.a.slavchev@gmail.com>
2022-03-26 16:35:12 +0000

    Minor design issues

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -16,41 +16,72 @@
         </a>
     </div>
 </div>
-
 <div class="row">
-    <div class="col-md-2"><strong>Име на залата:</strong></div>
-    <div class="col-md-10">{{$room->room_name}}</div>
-</div>
-<div class="row">
-    <div class="col-md-2"><strong>Капацитет:</strong></div>
-    <div class="col-md-10">{{$room->capacity}} студенти</div>
-</div>
-<div class="row">
-    <div class="col-md-2"><strong>Интернет достъп:</strong></div>
-    <div class="col-md-10">{{config('enums.rooms_internet')[$room->internet]}}</div>
-</div>
-<div class="row">
-    <div class="col-md-2"><strong>Мултимедия:</strong></div>
-    <div class="col-md-10">{{config('enums.rooms_multimedia')[$room->multimedia]}}</div>
-</div>
-@if ($room->notes)    
-    <div class="row">
-        <div class="col-md-2"><strong>Забележки:</strong></div>
-        <div class="col-md-10">{{$room->notes}}</div>
-    </div>
-@endif
-<div class="row">
-    <div class="col-md-2"><Strong>Дисциплини в залата:</Strong></div>
-    <div class="col-md-10">
-        @foreach ($room->seminars->unique('subject_id') as $seminar)
-            <div class="row border-bottom">
-                <div class="col-md-8">
-                    {{$seminar->subject->name.' ('.$seminar->subject->code.')'}}
-                </div>
-                <div class="col-md-4">
-                    {{config('enums.acad_positions')[$seminar->academic->acad_position].' '.$seminar->academic->first_name.' '.$seminar->academic->last_name.' '.config('enums.acad_titles')[$seminar->academic->acad_title]}}
-                </div>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-6 col-md-5"><strong>Име на залата:</strong></div>
+            <div class="col-6 col-md-7">{{$room->room_name}}</div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-5"><strong>Капацитет:</strong></div>
+            <div class="col-6 col-md-7">{{$room->capacity}} студенти</div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-5"><strong>Интернет достъп:</strong></div>
+            <div class="col-6 col-md-7">{{config('enums.rooms_internet')[$room->internet]}}</div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-md-5"><strong>Мултимедия:</strong></div>
+            <div class="col-6 col-md-7">{{config('enums.rooms_multimedia')[$room->multimedia]}}</div>
+        </div>
+        @if ($room->notes)    
+            <div class="row">
+                <div class="col-6 col-md-5"><strong>Забележки:</strong></div>
+                <div class="col-6 col-md-7">{{$room->notes}}</div>
             </div>
+        @endif
+        <div class="row">
+            <div class="col"><Strong>Дисциплини в залата:</Strong></div>
+        </div>
+            @foreach ($room->seminars->unique('subject_id') as $seminar)
+                <div class="row border-top">
+                    <div class="col-8 col-md-8">
+                        {{$seminar->subject->name.' ('.$seminar->subject->code.')'}}
+                    </div>
+                </div>
+            @endforeach
+    </div>
+    <div class="col-md-6">
+        <div class="row"><h5>Програма на залата днес:</h5></div>
+        <div class="row border-bottom">
+            <div class="col-3">
+                <strong>Час</strong>
+            </div>
+            <div class="col-2">
+                <strong>Група</strong>
+            </div>
+            <div class="col-3">
+                <strong>Дисциплина</strong>
+            </div>
+            <div class="col-4">
+                <strong>Преподавател</strong>
+            </div>
+        </div>
+        @foreach ($room->seminars->where('date', '=', date('Y-m-d'))->sortBy('date')->sortBy('period') as $seminar)
+        <div class="row border-bottom">
+            <div class="col-md-3">
+                {{config('enums.class_periods')[$seminar->period]}}
+            </div>
+            <div class="col-md-2">
+                {{$seminar->student_group->name}}
+            </div>
+            <div class="col-md-3">
+                {{$seminar->subject->code}}
+            </div>
+            <div class="col-md-4">
+                {{config('enums.acad_positions')[$seminar->academic->acad_position].' '.$seminar->academic->last_name}}
+            </div>
+        </div>    
         @endforeach
     </div>
 </div>
