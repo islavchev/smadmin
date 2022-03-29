ec00f5b5f5fd17ab8727b40ec4ec1bc81e28dea4
Ivan Sl <i.a.slavchev@gmail.com>
2022-03-29 18:54:02 +0000

    Fix design issues on semester.select

--- resources/views/semester/choose.blade.php
+++ resources/views/semester/choose.blade.php
@@ -10,23 +10,27 @@
         @csrf
         @method('PUT')
         <div class="row mt-5">
-            <h5>Изберете година и семестър</h5>
+            <div class="col-12 col-md-6 col-lg-4">
+                <h5>Изберете година и семестър</h5>
+            </div>
         </div>
         <div class="row my-2">
-            <div class="col-6 col-md-2">
+            <div class="col-6 col-md-3 col-lg-2">
                 <input type="number" name="year" id="year" value="{{date('Y')}}" min="1990" max="2200" class="form-control form-control-sm">
             </div>
-            <div class="col-6 col-md-2">
+            <div class="col-6 col-md-3 col-lg-2">
                 <select name="semester" id="semester" class="form-select form-select-sm">
                     <option value="1">Летен</option>
                     <option value="2">Зимен</option>
                 </select>
             </div>
         </div>
-        <div class="row justify-content-center">
-            <button type="submit" class="btn btn-success btn-sm mt-3 col-11 col-md-4">
-                Покажи
-            </button>  
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 d-grid">
+                <button type="submit" class="btn btn-block btn-success btn-sm mt-3">
+                    Покажи
+                </button>  
+            </div>
         </div>  
     </form>
 
